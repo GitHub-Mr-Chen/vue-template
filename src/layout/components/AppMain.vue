4c9646241b5b24f74818f05815bbda4dc60ca55c
陈威 <wei.chen@arbaseworld.com>
2020-03-07 23:16:20 +0800

    测试

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,5 @@
+<template>
+	<!-- <transition enter-active-class="animated fadeIn faster" leave-active-class="animated fadeIn faster"> -->
+	<router-view></router-view>
+	<!-- </transition> -->
+</template>
