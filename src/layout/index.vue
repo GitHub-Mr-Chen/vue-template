3f0b068d04f619f663942f90aaa69e2aa4cd9911
陈威 <wei.chen@arbaseworld.com>
2020-03-07 23:16:34 +0800

    测试

---
+++ src/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<Titlebar></Titlebar>
+		<AppMain></AppMain>
+	</div>
+</template>
+<script>
+import AppMain from './components/AppMain'
+import Titlebar from './components/Titlebar'
+export default {
+    'name': 'Layout',
+    'components': {
+        AppMain,
+        Titlebar
+    }
+}
+</script>
